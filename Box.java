76f3a800f21acc3bf720f49825c686bbf0a4ec18
Thieskeloo <thijsvanloo4@gmail.com>
2018-01-08 15:34:02 +0100

    added getters

--- Box.java
+++ Box.java
@@ -1,27 +1,40 @@
 public class Box {
 
-  private double height;
-  private double length;
-  private double width;
+	private double height;
+	private double length;
+	private double width;
 
-  private double value;
+	private double value;
 
-  public Box(double aHeight, double aLength, double aWidth) {
-    height = aHeight;
-    length = aLength;
-    width = aWidth;
-    value = 0;
-  }
+	public Box(double aHeight, double aLength, double aWidth) {
+		height = aHeight;
+		length = aLength;
+		width = aWidth;
+		value = 0;
+	}
+	public Box(double aHeight, double aLength, double aWidth, double aValue) {
+		height = aHeight;
+		length = aLength;
+		width = aWidth;
+		value = aValue;
+	}
+	public double getValuePerUnit() {
+		return (((value/height)/length)/width);
+	}
+	
+	public double getHeight() {
+		return height;
+	}
 
-  public Box(double aHeight, double aLength, double aWidth, double aValue) {
-    height = aHeight;
-    length = aLength;
-    width = aWidth;
-    value = aValue;
-  }
+	public double getLength() {
+		return length;
+	}
 
-  public double getValuePerUnit() {
-    return (((value/height)/length)/width);
-  }
+	public double getWidth() {
+		return width;
+	}
 
+	public double getValue() {
+		return value;
+	}
 }
