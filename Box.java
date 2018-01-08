af48aab99982d7572945db27be82635a0b9f2151
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-01-08 15:18:10 +0100

    added Box

---
+++ Box.java
@@ -0,0 +1,27 @@
+public class Box {
+
+  private double height;
+  private double length;
+  private double width;
+
+  private double value;
+
+  public Box(double aHeight, double aLength, double aWidth) {
+    height = aHeight;
+    length = aLength;
+    width = aWidth;
+    value = 0;
+  }
+
+  public Box(double aHeight, double aLength, double aWidth, double aValue) {
+    height = aHeight;
+    length = aLength;
+    width = aWidth;
+    value = aValue;
+  }
+
+  public double giveValuePerUnit() {
+    return (((value/height)/length)/width);
+  }
+
+}
