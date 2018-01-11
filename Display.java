8de48ba8c7f3e3403bcda1123186a40a16303667
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-01-11 16:22:05 +0100

    fixed your code

--- Display.java
+++ Display.java
@@ -11,7 +11,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 /*
-This class takes the objects recieved from the PlaceBox class as a parameter
+This class takes the objects recieved from the PlacedBox class as a parameter
 and then visually represents them in three dimensions
 */
 public class Display {
@@ -19,7 +19,7 @@ public class Display {
 	//xCamera and yCamera control the viewing angle of the cargo
 	private int xCamera = -6;
 	private int yCamera =  4;
-	
+
 	//Constructer that takes in an array filled with objects from PlacedBox
 	public Display(PlacedBox[] boxArray){
 		this.boxArray = boxArray;
@@ -27,26 +27,26 @@ public class Display {
 		GUI g = new GUI();
 	}
 	/*
-	This method takes the boxArray array and goes through each index of boxArray 
-	and visually represents each object in the indexes. It also assigns each object in 
-	boxArray a certain color depending on what package they are. 
+	This method takes the boxArray array and goes through each index of boxArray
+	and visually represents each object in the indexes. It also assigns each object in
+	boxArray a certain color depending on what package they are.
 	*/
 	private void print3DArray(){
-		//creates the area in which the boxes are created 
+		//creates the area in which the boxes are created
 		SimpleUniverse universe = new SimpleUniverse();
 		BranchGroup group = new BranchGroup();
-		
+
 		//sets the appearence that the coloringAttributes will be passed into for A,B, and C boxes
 		Appearance appearanceA = new Appearance();
 		Appearance appearanceB = new Appearance();
 		Appearance appearanceC = new Appearance();
-		
+
 		//sets the coloringAttributes that color3f will be passed into for A,B, and C boxes
 		ColoringAttributes coloringAttributesA = new ColoringAttributes();
 		ColoringAttributes coloringAttributesB = new ColoringAttributes();
 		ColoringAttributes coloringAttributesC = new ColoringAttributes();
-		
-		//sets the color of the boxes 
+
+		//sets the color of the boxes
 		Color3f colorA = new Color3f(Color.yellow);
 		Color3f colorB = new Color3f(Color.red);
 		Color3f colorC = new Color3f(Color.green);
@@ -57,7 +57,7 @@ public class Display {
 		appearanceA.setColoringAttributes(coloringAttributesA);
 		appearanceB.setColoringAttributes(coloringAttributesB);
 		appearanceC.setColoringAttributes(coloringAttributesC);
-		
+
 		/*
 		This for loop goes throgh the indexes of boxArray and looks to see if they are of A,B, or C type,
 		it then assigns their color and visually represents them in 3D
@@ -99,7 +99,7 @@ public class Display {
 		rotation.mul(viewTransform);
 		universe.getViewingPlatform().getViewPlatformTransform().setTransform(rotation);
 		universe.getViewingPlatform().getViewPlatformTransform().getTransform(viewTransform);
-		
+
 		//This sets the lighting of our universe so our objects are visible
 		Color3f light1Color = new Color3f(.1f, 1.4f, .1f); // green light
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
@@ -107,14 +107,14 @@ public class Display {
 		DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
 		light1.setInfluencingBounds(bounds);
 		group.addChild(light1);
-		
+
 		// adds the group of objects to the Universe
 		universe.addBranchGraph(group);
 	}
-		
-		
+
+
 		class GUI extends JFrame{
-			/* 
+			/*
 			This class creates a seperate JFrame that is our user interface
 			using buttons to change the viewing angle of the objects that
 			were represented in 3D
@@ -125,69 +125,69 @@ public class Display {
 			private JButton moveRight;
 			public GUI(){
 				createComponent();
-				
+
 				this.setSize(frameWidth, frameHeight);
 				this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				this.setTitle("Control Unit");
 				this.setLocationRelativeTo(null);
 				this.setVisible(true);
-				
-				
+
+
 			}
-			
+
 			public void createComponent(){
 				createButtons();
 				createRadio();
-				
+
 				JPanel panel = new JPanel();
 				panel.setLayout(new BorderLayout());
-				
+
 				panel.add(moveLeft, BorderLayout.WEST);
 				panel.add(moveRight, BorderLayout.EAST);
 				this.add(panel);
 			}
-			
+
 			public void createButtons(){
 				 moveLeft = new JButton("Left 90");
 				 moveRight = new JButton("Right 90");
-				
+
 				moveLeft.addActionListener(new ActionListener() {
-					
+
 					public void actionPerformed(ActionEvent e){
 						yCamera = yCamera + 8;
 						xCamera = xCamera + 40;
 						print3DArray();
-		
+
 				}
 					});
-					
+
 				moveRight.addActionListener(new ActionListener() {
-			
+
 					public void actionPerformed(ActionEvent e){
 						yCamera = yCamera - 8;
 						xCamera = xCamera - 40;
 						print3DArray();
-		
+
 				}
 					});
-		
+
 			}
-			
+
 			public void createRadio(){
-				
+
 			}
-		
-			
+
+
 		}
 
-		
+
 	public static void main(String[] args){
 		PlacedBox box = new PlacedBox( 3,2,2,'a',1,2,1);
 		PlacedBox box1 = new PlacedBox( 1,2,1,'a',1,5,6);
 		PlacedBox box2 = new PlacedBox( 1,1,1,'a',1,1,1);
 		PlacedBox[] array = {box,box1,box2};
-		
-		
+
+
 		Display display = new Display(array);
 	}
 }
