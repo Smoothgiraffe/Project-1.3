a57d928d02539c0f52a140b667ea014c3c1789b2
Smoothgiraffe <32669155+Smoothgiraffe@users.noreply.github.com>
2018-01-11 15:31:52 +0100

    Added Pentomino and PlacedPentomino + their respective functions
    Basically the same as the boxes with the difference that the pentominoes are able to return the 3D array that is put into the cargo space
    The pentominoes can also rotate themselves and flip (hopefully this works!)

---
+++ Pentomino.java
@@ -0,0 +1,93 @@
+public class Pentomino {
+
+  protected double value;
+  protected char name;
+  protected final double volume = 2.5; //0.5*5 = 2.5
+
+  char[][][] pentP = {{{'p','p'},{'p','p'},{'p','\u0000'}}};
+  char[][][] pentL = {{{'l','\u0000','\u0000','\u0000',},{'l','l','l','l'}}};
+  char[][][] pentT = {{{'t','t','t'},{'\u0000','t','\u0000'},{'\u0000','t','\u0000'}}};
+
+  public Pentomino(char aName) {
+    name = aName;
+  }
+
+  public Pentomino(char aName, double aValue) {
+    name = aName;
+    value = aValue;
+  }
+
+  public double getVolume() {
+    return volume;
+  }
+
+  public char[][][] getPentP() {
+    return pentP;
+  }
+
+  public char[][][] getPentL() {
+    return pentL;
+  }
+
+  public char[][][] getPentT() {
+    return pentT;
+  }
+
+  public char[][][] rotate(char[][][] pent, int rotation) {
+    if(rotation == 0) {
+      return pent;
+    } else if(rotation == 1) {
+      char[][][] rotatedPent = new char[pent.length][pent[0][0].length][pent[0].length];
+      for(int i = 0; i < pent.length; i++) {
+        for(int j = 0; j < pent[0][0].length; j++) {
+          for(int k = 0; k < pent[0].length; k++) {
+            rotatedPent[i][j][k] = pent[i][k][j];
+          }
+        }
+      }
+      return rotatedPent;
+    } else if(rotation == 2) {
+      char[][][] rotatedPent = new char[pent[0].length][pent.length][pent[0][0].length];
+      for(int i = 0; i < pent[0].length; i++) {
+        for(int j = 0; j < pent.length; j++) {
+          for(int k = 0; k < pent[0][0].length; k++) {
+            rotatedPent[i][j][k] = pent[j][i][k];
+          }
+        }
+      }
+      return rotatedPent;
+    } else if(rotation == 3) {
+      char[][][] rotatedPent = new char[pent[0].length][pent[0][0].length][pent.length];
+      for(int i = 0; i < pent[0].length; i++) {
+        for(int j = 0; j < pent[0][0].length; j++) {
+          for(int k = 0; k < pent.length; k++) {
+            rotatedPent[i][j][k] = pent[j][k][i];
+          }
+        }
+      }
+      return rotatedPent;
+    } else if(rotation == 4) {
+      char[][][] rotatedPent = new char[pent[0][0].length][pent.length][pent[0].length];
+      for(int i = 0; i < pent[0][0].length; i++) {
+        for(int j = 0; j < pent.length; j++) {
+          for(int k = 0; k < pent[0].length; k++) {
+            rotatedPent[i][j][k] = pent[k][i][j];
+          }
+        }
+      }
+      return rotatedPent;
+    } else if(rotation == 5) {
+      char[][][] rotatedPent = new char[pent[0][0].length][pent[0].length][pent.length];
+      for(int i = 0; i < pent[0][0].length; i++) {
+        for(int j = 0; j < pent[0].length; j++) {
+          for(int k = 0; k < pent.length; k++) {
+            rotatedPent[i][j][k] = pent[k][j][i];
+          }
+        }
+      }
+      return rotatedPent;
+    }
+    return null;
+  }
+
+}
---
+++ PlacedPentomino.java
@@ -0,0 +1,35 @@
+public class PlacedPentomino extends Pentomino{
+  private int placedX;
+  private int placedY;
+  private int placedZ;
+
+  //Constructor without value
+  public PlacedPentomino(char aName, int aPlaceX, int aPlaceY, int aPlaceZ) {
+    super(aName);
+    placedX = aPlaceX;
+    placedY = aPlaceY;
+    placedZ = aPlaceZ;
+
+  }
+
+  //Constructor with value
+  public PlacedPentomino(char aName, double aValue, int aPlaceX, int aPlaceY, int aPlaceZ) {
+    super(aName, aValue);
+    placedX = aPlaceX;
+    placedY = aPlaceY;
+    placedZ = aPlaceZ;
+
+  }
+
+  public int getX() {
+    return placedX;
+  }
+
+  public int getY() {
+    return placedY;
+  }
+
+  public int getZ() {
+    return placedZ;
+  }
+}
