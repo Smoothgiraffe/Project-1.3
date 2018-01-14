2046403fafd25c305f0dec845d53f3c8112044f7
Thieskeloo <thijsvanloo4@gmail.com>
2018-01-14 14:25:18 +0100

    added penttobox start

--- PentToBox.java
+++ PentToBox.java
@@ -3,9 +3,14 @@ import java.util.ArrayList;
 public class PentToBox {
 
 	public ArrayList<PlacedBox> convert(ArrayList<PlacedPentomino> pentArray){
-		ArrayList<PlacedBox> = new ArrayList<PlacedBox>();
-		for (int i = 0; i < pentArray.size(); i++) {
+		ArrayList<PlacedBox> pentArray = new ArrayList<PlacedBox>();
 
+		for (int i = 0; i < pentArray.size(); i++) {
+			PlacedPentomino pent = pentArray.get(i);
+
+			char[][][] pentArray = pent.getPentomino();
+
+			for (int i; i < )
 		}
 	}
 }
