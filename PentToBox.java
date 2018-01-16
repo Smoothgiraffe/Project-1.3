ebe3c6ce371887ceaadaf42ffb7fa916974ec86b
Thieskeloo <thijsvanloo4@gmail.com>
2018-01-16 10:49:45 +0100

    some updates

--- PentToBox.java
+++ PentToBox.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class PentToBox {
 
 	public ArrayList<PlacedBox> convert(ArrayList<PlacedPentomino> pentArray){
-		ArrayList<PlacedBox> pentArray = new ArrayList<PlacedBox>();
+		//ArrayList<PlacedBox> pentArray = new ArrayList<PlacedBox>();
 
 		/*for (int i = 0; i < pentArray.size(); i++) {
 			PlacedPentomino pent = pentArray.get(i);
